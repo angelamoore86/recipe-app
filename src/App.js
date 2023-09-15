f62957d84b47ceba5fed7c20f96e70957e888907
angelamoore86 <122281605+angelamoore86@users.noreply.github.com>
2023-09-15 16:15:33 -0230

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react';
+import { RecipeList } from './Recipes';
 
 function App() {
+  let recipeData = [ {name: "Tacos", ingredients: "", directions: "", description: ""}, {name: "Stew", ingredients: "", directions: "", description: ""}];
+
+  const [recipes, setRecipes] = useState(null);
+
+  useEffect( () => {
+    setRecipes(recipeData)
+  }, [])
+
+  if (recipes == null) return;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Recipes!</h1>
+      <RecipeList recipes={recipes}/>
     </div>
   );
 }
