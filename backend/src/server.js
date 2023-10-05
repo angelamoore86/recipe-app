f4d34bea70571f88cb8fb72f5c6a6bbf7c906512
angelamoore86 <122281605+angelamoore86@users.noreply.github.com>
2023-10-05 18:07:53 -0230

    Created file and code for backend

---
+++ backend/src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { db, connectToDb } from './db.js';
+import multer from 'multer';
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "/images");
+    },
+    filename: (req, file, cb) => {
+        cb(file.originalname);
+    },
+});
+
+const upload = multer({storage});
+
+app.get('/api/recipes', async (req, res) => {
+    const recipeData = await db.collection('recipes').find({}).toArray();
+    res.json(recipeData);
+});
+
+app.post('/api/form', upload.single('image'), async (req, res) => {
+    const {name, ingredients, directions, description} = req.body;
+    const image = req.file.filename;
+
+    const result = await db.collection('recipes').insertOne({name, ingredients,
+         directions, description, image });
+    if(result.insertedCount === 1){
+        console.log("Recipe was added.");
+        res.status(201).json(recipe);
+    } else {
+        console.error("Could not add recipe.");
+        res.status(500).json(recipe);
+    }
+    
+});
+
+app.post('/api/removeRecipe', async (req, res) => {
+    const result = await db.collection('recipes').deleteOne({name:req.body.recipename});
+    const deletedRecipe = await db.collection('recipes').find({}).toArray();
+    
+    res.json(deletedRecipe);
+});
+
+connectToDb(() => {
+    console.log("Successfully connected to database.")
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
+});
